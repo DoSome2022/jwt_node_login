5a0f90f2e9ab16db1658819da9b796347d056121
Tim <lhpbtk@gamil.com>
2023-05-11 07:04:00 +0800

    在mainv2 11-5-2023

---
+++ Controller/Auth.js
@@ -0,0 +1,15 @@
+export const GetLogin = (req,res)=>{
+    res.render();
+}
+
+export const GetRegister = (req,res)=>{
+    res.render();
+}
+
+export const PostLogin = (req,res)=>{
+
+}
+
+export const PostRegister = (req,res)=>{
+
+}
--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { GetLogin , GetRegister , PostLogin , PostRegister } from "../Controller/Auth.js"
 const Routes =express.Router();
 
 
-Routes.route('/login')
-Routes.route('/register')
+Routes.route('/login').get(GetLogin).post(PostLogin)
+Routes.route('/register').get(GetRegister).post(PostRegister)
 
 export default Routes
